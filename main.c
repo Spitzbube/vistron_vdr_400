3c1a176806f9b94e16d46eaa33cacbfea67b2516
Spitzbube <spitzbube@mail.bg>
2022-06-11 13:45:01 +0200

    Blinking of LED D3 without macros

--- main.c
+++ main.c
@@ -14,24 +14,20 @@ void delay(uint32_t msec)
 int main(void)
 {
     // Enable Port E and alternate functions
-    SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPEEN);
-
-    // PE5 = Output for LED D2
-    MODIFY_REG(GPIOE->CRL, GPIO_CRL_CNF5  + GPIO_CRL_MODE5,  GPIO_CRL_MODE5_0);
+    RCC->APB2ENR |= RCC_APB2ENR_IOPEEN;
 
     // PE6 = Output for LED D3
-    MODIFY_REG(GPIOE->CRL, GPIO_CRL_CNF6  + GPIO_CRL_MODE6,  GPIO_CRL_MODE6_0);
+    GPIOE->CRL &= ~(GPIO_CRL_CNF6 + GPIO_CRL_MODE6);
+    GPIOE->CRL |= GPIO_CRL_MODE6_0;
 
     while(1)
     {
         // LED Pin -> High
-        WRITE_REG(GPIOE->BSRR, GPIO_BSRR_BR5);
-        WRITE_REG(GPIOE->BSRR, GPIO_BSRR_BS6);
+        GPIOE->BSRR |= GPIO_BSRR_BS6;
         delay(500);
 
         // LED Pin -> Low
-        WRITE_REG(GPIOE->BSRR, GPIO_BSRR_BS5);
-        WRITE_REG(GPIOE->BSRR, GPIO_BSRR_BR6);
+        GPIOE->BSRR |= GPIO_BSRR_BR6;
         delay(500);
     }
 }
